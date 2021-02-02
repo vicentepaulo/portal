cb398a21018e7d963276f46e93406073aa52d917
Vicente Paulo de Souza Júnior <59590810+vicentepaulo@users.noreply.github.com>
2021-02-02 20:58:51 -0300

    commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/reset.css">
+    <link href="./css/normalize.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/menuDeNavegacao.css">
+    <link rel="stylesheet" href="./css/menuBurger.css">
+
+
+    <title>index</title>
+</head>
+
+<body>
+    <div class="bordaCabecalho">
+      
+    <ul class="menu">
+        <!-- Esse é o 1 nivel ou o nivel principal -->
+        <li><a href="#"><img id="menuBurguer" alt="Logotipo Inovamed" src="./img/icon/iconeMenuBurgerBranco.png"></a> 
+            <ul class="submenu-1">
+
+                <!-- Esse é o 2 nivel ou o primeiro Drop Down --> 
+                <li><a href="#">Avarias</a></li>           
+                <li>
+                    <a href="#">Solicitações</a>
+                </li>       
+                <li><a href="#">Chamados</a>            
+                    <ul class="submenu-2">
+
+                        <!-- Esse é o 3 nivel ou o Segundo Drop Down -->       
+                        <li><a href="#">Novo Chamado</a></li>       
+                        <li><a href="#">Meus Chamados</a></li>       
+                        <li><a href="#"></a>                       
+                            <!--<ul class="submenu-3">
+
+                                <li><a href="#">7</a>
+                                </li>
+                                <li><a href="#">8</a></li>
+                                <li><a href="#">9</a></li>    -->
+                    </ul>                       
+                </li>       
+            </ul>   
+        </li>       
+    </ul> 
+    </div>
+    <img class="logo" alt="Logotipo Inovamed" src="./img/logo/logoPositiva.png">
+    <div class="bordaDireita"></div>
+</body>
+
+</html>
+
+
+
